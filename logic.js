01df99d46f0d1b99a53077024f5202c2bf896fb7
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-08-08 11:01:50 +0500

    Updated

--- logic.js
+++ logic.js
@@ -6,14 +6,12 @@ window.speechSynthesis.onvoiceschanged = ()=>{
     voices =  window.speechSynthesis.getVoices();
     speech.voice = voices[0];
     voices.forEach((voice , i) =>(voiceSelect.options[i] = new Option(voice.name , i)));
-
 };
 
 voiceSelect.addEventListener('change' , ()=>{
     speech.voice = voices[voiceSelect.value];
 })
 document.querySelector('button').addEventListener('click' , () =>{
-
     speech.text = document.querySelector('textarea').value;
     window.speechSynthesis.speak(speech);
 
